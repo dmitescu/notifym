547e122b45c8ef5da03b172231528987744cace8
mihber <mihaibrk99@yahoo.ro>
2016-01-17 15:14:15 +0200

    Update notifym.pl

--- notifym.pl
+++ notifym.pl
@@ -1,6 +1,6 @@
 #
 # Copyright (c) 2016 Mitescu George Dan <mitescugeorgedan@gmail.com>
-# Copyright (c) 2016 Berechet Mihai <>
+# Copyright (c) 2016 Berechet Mihai <mihaibereket9954@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
