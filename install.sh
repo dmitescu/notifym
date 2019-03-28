c146c265482d22dd430dcb49b0230f80505dd1b8
Mitescu George Dan <mitescugd@gmail.com>
2019-03-28 11:52:44 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
 
-cp notifym.pl ~/.weechat/perl/notifym.pl
+USAGE=<<EOF
+Please set $WEECHAT_HOME or create ~/.weechat/ to install.
+EOF
+
+if [ ! -z $WEECHAT_HOME ]; then
+    cp notifym.pl $WEECHAT_HOME/perl/notifym.pl
+elif [ -d $HOME/.weechat/ ]; then
+    mkdir -p $HOME/.weechat/perl
+    cp notifym.pl $HOME/.weechat/perl/notifym.pl
+else
+    echo $USAGE
+    exit 1
+fi
